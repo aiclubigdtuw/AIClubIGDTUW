a733a169e046aff7e6d0064b3d8ffec2406dfbc9
trisha-tomy <trishatomy3.14@gmail.com>
2023-09-02 22:27:21 +0530

    initial layout done

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const port = 4004
-const teamData = require("./public/JS/team-data")
+const path = require("path");
+const port = 4004;
+const teamData = require("./public/JS/team-data");
 //view engine setup
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, '/views'))
-app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/register', (req, res) => {
-    res.render('register');
-})
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
-app.get('/team', (req, res) => {
-    res.render('team', teamData);
-})
+app.get("/team", (req, res) => {
+  res.render("team", teamData);
+});
+app.get("/public/css/team.css", (req, res) => {
+  res.setHeader("Content-Type", "text/css");
+  res.sendFile(path.join(__dirname, "public", "css", "team.css"));
+});
 
-app.get('/events', (req, res) => {
-    res.render('events');
-})
+app.get("/events", (req, res) => {
+  res.render("events");
+});
 
 // app.get('/tad', (req, res) => {
 //     res.render('tad');
 // })
 
 app.listen(process.env.PORT || port, () => {
-    console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
